32a141c8d4c0ce76dbe0809d605fbc821c6c190b
Kristoffer Årvik <kristova@stud.ntnu.no>
2018-07-17 19:44:36 +0200

    Interrim changes

--- DataCollection.py
+++ DataCollection.py
@@ -28,17 +28,12 @@ import json
 
 
 def GetOSETickersAndNames():
-     """I manually cleaned the data. It is probably pretty easy to do it directly but it served my purpose. Check the repo for a ticker-list if you don't want the hassle"""
-    #TODO - Create an actual parser Parser
+#I manually cleaned the data. It is probably pretty easy to do it directly but it served my purpose. Check the repo for a ticker-list if you don't want the hassle"""
+#TODO - Create an actual parser Parser
     TURL="https://www.oslobors.no/ob/servlets/components?type=table&generators%5B0%5D%5Bsource%5D=feed.ose.quotes.EQUITIES%2BPCC&filter=VOLUME_TOTAL%3En0&view=DELAYED&columns=ITEM_SECTOR%2C+ITEM%2C+LONG_NAME%2C&channel=5571c881cdd34770e386931bddb48f05"
     TickerRaw=p.read_json(TURL)
     T=TickerRaw.to_csv('TickersAndNames',index=False)
     
-   
-  
-  
-    
-
 
 def DataCollectionNetFonds(Ticker):
     print("Henter data for " + Ticker)
@@ -62,3 +57,11 @@ def DeltaDataCollectionNetFonds(Ticker, LastDate): #TODO - Use the Lastdate para
     c=p.read_csv(io.StringIO(s.decode("ISO-8859-1")))
     print(c)
 
+def GetHistoricalDataforTickers():
+    MyTickerList='D:\Programmering\TradingAnalysisTool\FinalTickersAndNames.csv'
+    tickertable=p.read_csv(MyTickerList, index_col='ID')
+    tickertable.loc('ID')
+    print(tickertable)
+    #print(tickers)
+    
+GetHistoricalDataforTickers()
